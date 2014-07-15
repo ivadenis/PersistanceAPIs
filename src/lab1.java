77eed1542c40ea02a8e25e23f471576aa6921b05
Ian Goegebuer <iangoegebuer@gmail.com>
2014-07-15 14:26:18 +0200

    Added library to project

--- src/lab1.java
+++ src/lab1.java
@@ -2,6 +2,9 @@
  * Created by ivadenis on 7/15/14.
  * Modified by Ian Goegebuer on 7/15/14
  */
+
+import javax.persistence.Entity;
+
 public class lab1 {
     public static void main(String[] args) {
 
